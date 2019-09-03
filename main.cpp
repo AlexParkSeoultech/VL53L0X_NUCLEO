4c8983a5facfd23bd2828d2f5f07d87115ad4218
HEE JAE PARK <30435157+AlexParkSeoultech@users.noreply.github.com>
2019-09-03 22:46:03 +0900

    First upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+
+#include "mbed.h"
+#include "VL53L0X.h"
+Serial pc(USBTX,USBRX, 115200);
+VL53L0X sensor(I2C_SDA, I2C_SCL,D12);
+DigitalOut led(LED1);
+void setPerform();
+int main(){
+    sensor.init();
+    sensor.setTimeout(500);
+    sensor.setApplication(VL53L0X::SHORT_ACCURATE);
+    sensor.startContinuous();
+    while(1) {
+        led = !led; // Toggle LED
+        pc.printf("Range %d ", sensor.readRange());
+        if (sensor.timeoutOccurred()) { pc.printf("TIMEOUT !!!"); }
+        pc.printf("\n");
+        wait(0.02);
+    }
+}
